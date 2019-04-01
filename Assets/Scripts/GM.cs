1520a1b38a154e000b04b9b92ea34a81b25bddac
Manuel Hidalgo Lorente <manuhida@ucm.es>
2019-04-01 22:38:15 +0200

    # Se empiezan script

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour
+{
+    #region Atributos
+
+    /// <summary>
+    /// Manejado general del juego.
+    /// </summary>
+    public static GM Gm { get; set; }
+
+    #endregion
+
+    #region Eventos
+
+    private void Awake()
+    {
+        if (Gm == null)
+        {
+            Gm = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Gm != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    #endregion
+
+    #region Métodos públicos
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    /**
+     * Método para el botón salir del menú
+     */
+    public void DoExitGame()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+}
