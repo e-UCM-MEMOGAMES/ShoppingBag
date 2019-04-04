b47867d10618d3e8aa725d576090afd4e9e58010
Manuel Hidalgo Lorente <manuhida@ucm.es>
2019-04-04 22:35:24 +0200

    + Se añade mensajes finales

--- Assets/Scripts/Result.cs
+++ Assets/Scripts/Result.cs
@@ -51,16 +51,48 @@ namespace Assets.Scripts
 
         #endregion
 
-        // Start is called before the first frame update
+        #region Eventos
+
         void Start()
         {
+            string correctList = GM.Gm.CorrectListResult();
+            string wrongList = GM.Gm.WrongListResult();
 
+            if (string.IsNullOrWhiteSpace(wrongList))
+                PutTextAll("¡Enhorabuena! ¡Acertaste toda la lista de la compra!");
+            else if (string.IsNullOrWhiteSpace(correctList))
+                PutTextAll("¡Que mal! ¡Fallaste toda la lista de la compra! No te rindas :)");
+            else
+            {
+                CorrectoTitle.enabled = true;
+                CorrectoList.enabled = true;
+                CorrectoList.text = correctList;
+                ErroneoTitle.enabled = true;
+                ErroneoList.enabled = true;
+                ErroneoList.text = wrongList;
+                All.enabled = false;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        #endregion
+
+        #region Métodos privados
+
+        /// <summary>
+        /// Se desactiva todos los campos y se pone el mensaje general.
+        /// </summary>
+        /// <param name="text">Texto a mostrar.</param>
+        private void PutTextAll(string text)
         {
-
+            CorrectoTitle.enabled = false;
+            CorrectoList.enabled = false;
+            ErroneoTitle.enabled = false;
+            ErroneoList.enabled = false;
+            All.enabled = true;
+            All.text = text;
         }
+
+        #endregion
+
     }
 }
